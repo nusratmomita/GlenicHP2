91741138d1004d0d0f7d2deb634a54ef9ac33d22
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-12-16 13:53:27 +0600

    Header and Banner added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// for banner slider
+var swiperBanner = new Swiper(".hero_banner_slider", {
+    navigation: {
+        nextEl: ".custom-swiper-btn_next",
+        prevEl: ".custom-swiper-btn_prev",
+    },
+});
